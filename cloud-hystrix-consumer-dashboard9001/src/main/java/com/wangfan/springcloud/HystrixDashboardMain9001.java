136aa98d743c826cf99da1fef59dbed762586891
wangfan <Fan.Wang.1984@outlook.com>
2022-06-13 17:20:44 +0800

    添加hystrix的可视化界面控制

---
+++ cloud-hystrix-consumer-dashboard9001/src/main/java/com/wangfan/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.wangfan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author wangfan
+ * @Description
+ * @date 2022/6/13 17:04:56
+ */
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
